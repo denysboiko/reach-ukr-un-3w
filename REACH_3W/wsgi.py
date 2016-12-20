17f43126ba31301efbdef4c6391adcb32012861c
denysboiko <denys.bojko@gmail.com>
2016-12-20 17:20:53 +0200

    data api

--- REACH_3W/wsgi.py
+++ REACH_3W/wsgi.py
@@ -11,6 +11,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "REACH_3W.settings")
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'REACH_3W.settings'
 
 application = get_wsgi_application()
