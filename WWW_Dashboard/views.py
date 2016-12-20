f198a3c36dc4878bf5cc8f579d3f80da7b6b18e5
denysboiko <denys.bojko@gmail.com>
2016-12-20 16:37:05 +0200

    db

--- WWW_Dashboard/views.py
+++ WWW_Dashboard/views.py
@@ -22,7 +22,7 @@ def ajax(request):
 def users_json(request):
 
     # conn = psycopg2.connect(database='3W_DB', user='postgres', password='3w_reach')
-    conn = psycopg2.connect(database='ebdb', user='postgres', password='django2016')
+    conn = psycopg2.connect(database='ebdb', user='postgres', password='django2016', host="aa7165x417nka7.cegqdc7fq3bu.eu-west-1.rds.amazonaws.com")
     cur = conn.cursor()
     cur.execute("""select row_to_json(t) from (select * from \"WWWData\" WHERE area_type = 'NGCA') t""")
 
