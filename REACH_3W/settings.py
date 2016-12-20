c52eb7377996a5f1f94c4fd9566c9488789ae4fb
denysboiko <denys.bojko@gmail.com>
2016-12-20 11:20:56 +0200

    static url

--- REACH_3W/settings.py
+++ REACH_3W/settings.py
@@ -175,4 +175,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'WWW_Dashboard','static')
+STATIC_ROOT = os.path.join(BASE_DIR, '..','static')
