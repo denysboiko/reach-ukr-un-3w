a66ae038c8e85d24cfa2762dfa237e377373a330
denysboiko <denys.bojko@gmail.com>
2016-12-27 11:29:42 +0200

    static root

--- REACH_3W/settings.py
+++ REACH_3W/settings.py
@@ -180,8 +180,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '..','static')
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'WWW_Dashboard', 'static')
+
+# ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
